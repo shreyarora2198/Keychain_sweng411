1557524a853053287b9158dbf8bc0c8f4249dfff
ChrisDro <christopherdrozynski@gmail.com>
2018-11-30 14:30:01 -0500

    Sync with John

--- platforms/android/app/src/main/assets/app/app/promotions/promotions.component.ts
+++ platforms/android/app/src/main/assets/app/app/promotions/promotions.component.ts
@@ -2,16 +2,28 @@ import { Component, OnInit } from "@angular/core";
 import * as app from "application";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 
+class DataItem {
+    constructor(public name: string) { }
+}
+
 @Component({
     selector: "Promotions",
     moduleId: module.id,
     styleUrls: ["./promotions.component.css"],
     templateUrl: "./promotions.component.html"
 })
+
 export class PromotionsComponent implements OnInit {
+    public promtnList: Array<DataItem>
+    private counter: number;
 
     constructor() {
-        // Use the component constructor to inject providers.
+        this.promtnList = [];
+        this.counter = 0;
+        for (var i = 0; i < 10; i++) {
+            this.promtnList.push(new DataItem("data item " + i));
+            this.counter = i;
+        }
     }
 
     ngOnInit(): void {
@@ -21,5 +33,11 @@ export class PromotionsComponent implements OnInit {
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    addPromotion(){
+        this.promtnList.push(new DataItem("data item " + this.counter));
+        this.counter++;
+        console.log("------------------------ ItemTapped: ");
     }
 }
