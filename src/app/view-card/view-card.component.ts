49042e0b03de92b584d46aaeaf3f1e90841b95c4
David Lengel <david@Davids-MBP.neo.rr.com>
2018-12-03 21:31:29 -0500

    Encode all barcode types in view page

--- src/app/view-card/view-card.component.ts
+++ src/app/view-card/view-card.component.ts
@@ -4,6 +4,8 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { Image } from "tns-core-modules/ui/image";
 import { TextField } from "ui/text-field";
 import { KeychainCardClass } from "../keychain-card";
+import { analytics } from 'nativescript-plugin-firebase';
+import { verticalAlignmentProperty } from 'tns-core-modules/ui/page/page';
 
 const ZXing = require("nativescript-zxing");
 const ImageSource = require("image-source");
@@ -24,6 +26,10 @@ export class ViewCardComponent implements OnInit {
     private cardLoc: string;
     private cardName: string;
 
+    barcodeImage: any;
+    zx: any;
+    newImg: any;
+
     constructor(private keychaincardclass: KeychainCardClass) {
         // Use the component constructor to inject providers.
         this.barcodeId = this.keychaincardclass.getBarcodeId();
@@ -33,15 +39,137 @@ export class ViewCardComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        console.log(this.barcodeId);
-        const barcodeImage = <Image>this.barcodeImg.nativeElement;
-        const zx = new ZXing();
-        const newImg = zx.createBarcode({
-            encode: this.barcodeId,//data
-            format: ZXing.UPC_A//format
-        });
-        barcodeImage.imageSource = ImageSource.fromNativeSource(newImg);
-    }
+        switch(this.barcodeFormat) {
+            case "QR_CODE":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.QR_CODE//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "UPC_A":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.UPC_A//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "UPC_E":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.UPC_E//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "AZTEC":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.AZTEC//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "CODABAR":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.CODABAR//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "CODE_39":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.CODE_39//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "CODE_93":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.CODE_93//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "CODE_128":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.CODE_128//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "DATA_MATRIX":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.DATA_MATRIX//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "EAN_8":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.EAN_8//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "EAN_13":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.EAN_13//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "ITF":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.ITF//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "MAXICODE":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.MAXICODE//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            case "PDF_417":
+                this.barcodeImage = <Image>this.barcodeImg.nativeElement;
+                this.zx = new ZXing();
+                this.newImg = this.zx.createBarcode({
+                    encode: this.barcodeId,//data
+                    format: ZXing.PDF_417//format
+                });
+                this.barcodeImage.imageSource = ImageSource.fromNativeSource(this.newImg);
+                break;
+            default:
+                console.log("Invalid barcode format");
+        }
+    }  
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
